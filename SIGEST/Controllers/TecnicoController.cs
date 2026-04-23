1f51d3125752c84e906a644b1c1201eb2abb02dd
Gustavo Jonapa <gustavo.jonapa24@unach.mx>
2026-04-23 14:17:37 -0600

    Agregar archivos de proyecto.

---
+++ SIGEST/Controllers/TecnicoController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SIGEST.Controllers
+{
+    public class TecnicoController : Controller
+    {
+        // GET: Tecnico
+        public ActionResult DashboardTecnico()
+        {
+            return View();
+        }
+        public ActionResult PerfilTecnico()
+        {
+            return View();
+        }
+    }
+}
---
+++ SIGEST/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SIGEST.MvcApplication" Language="C#" %>
